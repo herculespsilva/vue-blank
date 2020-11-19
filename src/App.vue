d06af52e9321a037cc21a75111eed72dd6a923ac
herculespsilva <herculespsilva@hotmail.com>
2020-11-19 00:46:34 +0000

    implements Persist and Actions

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,19 @@
 <template>
     <div id="app">
-        <p>Novo curso:
-            <input type="text" v-model="novo">
-            <button @click="incluir">Inserir</button>
-        </p>
-        <ul>
-            <app-curso-item v-for="(curso, indice) in cursos" @click="excluir(indice)" :key="indice" :value="curso.nome">
-                ({{indice}} - {{curso.nome}})
-            </app-curso-item>
-            <component :is="componente" value="Fixo">( 9999 )</component>
-        </ul>
+        <app-input />
+        <app-output />
     </div>
 </template>
 
 <script>
-    import CursoItem from './components/CursoItem';
+    import Input from './components/Input';
+    import Output from './components/Output';
 
     export default {
         name: 'app',
         components: {
-            'app-curso-item': CursoItem
-        },
-        data() {
-            return {
-                componente:  'app-curso-item',
-                novo: '',
-                cursos: [
-                    { nome: 'ADS'},
-                    { nome: 'Banco de Dados'}
-                ]
-            }
-        },
-
-        methods: {
-            incluir() {
-                if(this.novo.trim().length == 0) {
-                    alert('O nome do curso deve ser preenchido!')
-                }
-                else {
-                    this.cursos.push( { nome: this.novo } );
-                }
-            },
-            excluir(indice) {
-                this.cursos.splice(indice, 1);
-            }
+            'app-input': Input,
+            'app-output': Output
         }
     }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,47 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexPersist from 'vuex-persist'
 
 Vue.use(Vuex)
 
+const vuexPersist = new VuexPersist({ //plugin para manter os dados
+  key: 'my-app',
+  storage: localStorage
+})
+
 export default new Vuex.Store({
+  plugins: [
+    vuexPersist.plugin
+  ],
   state: {
+    numero1: 0,
+    numero2: 0
+  },
+  getters: {
+    soma (state) {
+      return state.numero1 * 1 + state.numero2 * 1
+    },
+    subtracao (state) {
+      return state.numero1 - state.numero2
+    },
+    multiplicacao: state => {
+      return state.numero1 * state.numero2
+    }
   },
   mutations: {
-  },
-  actions: {
+      setNumero1 (state, valor) {
+        state.numero1 = valor
+      },
+      setNumero2 (state, valor) {
+        state.numero2 = valor
+      }
+    },
+  actions: {       //metodo assíncrono = espera um metodo terminar sua execução para rodar
+      setNumero2Delay (context, valor) { //context é semelhante ao state, no entanto voltado para coisas assíncronas
+        setTimeout(() => {
+            context.commit('setNumero2', valor) // demora um segundo para salvar alteração - (Input/setNumero2Delay)
+        }, 1000)
+      }
   },
   modules: {
   }
